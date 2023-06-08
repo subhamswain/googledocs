bdfc9b690256cc829ebf83236698f94192c03509
subham swain <subham.swain@targetintegration.com>
2023-06-08 15:32:30 +0530

    googledocs2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import "./App.css";
 import Editor from "./Components/Editor/Editor";
 import NavBar from "./Components/NavBar/NavBar";
-import SideBar from "./Components/SideBar/SideBar";
 
 function App() {
     return (
         <div className="App">
             <NavBar />
             <Editor />
-            <SideBar />
+            
         </div>
     );
 }
